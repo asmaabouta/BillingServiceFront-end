0d15c9301a0c647c2abb0063c793848bba166719
asmaabouta <asmaabouta99@gmail.com>
2021-12-01 18:45:17 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AccueilComponent} from "./components/accueil/accueil.component";
+import {LoginComponent} from "./components/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './components/customer/customer/customer.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    AccueilComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
